c28b47c6084c67013eaf1ef4328fba3a533eac56
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2003-01-14 22:25:00 +0000

    Autohide the taskbar.
    (Old svn revision: 6457)

--- xftaskbar/xftaskbar.c
+++ xftaskbar/xftaskbar.c
@@ -3,18 +3,60 @@
 #include <libxfcegui4/libxfcegui4.h>
 
 #define FIXED_HEIGHT 26
+#define HIDDEN_HEIGHT 4
 
-int main(int argc, char **argv)
+typedef struct _Taskbar Taskbar;
+struct _Taskbar
 {
-    DesktopMargins margins;
-    SessionClient *client_session;
-    NetkScreen *screen;
+    guint x, y, width, height;
     GtkWidget *win;
     GtkWidget *frame;
     GtkWidget *hbox;
     GtkWidget *tasklist;
     GtkWidget *pager;
+    GtkWidget *eventbox;
+};
 
+static gboolean panel_enter (GtkWidget *widget, GdkEventCrossing *event, gpointer data)
+{
+    Taskbar *taskbar = (Taskbar *) data;
+  
+    if (event->detail != GDK_NOTIFY_INFERIOR)
+    {
+	gtk_widget_show (taskbar->frame);
+	gtk_widget_set_size_request(GTK_WIDGET(taskbar->win), taskbar->width, taskbar->height);
+	gtk_window_move(GTK_WINDOW(taskbar->win), taskbar->x, taskbar->y);
+    }
+  
+    return FALSE;
+}
+
+static gboolean panel_leave (GtkWidget *widget, GdkEventCrossing *event, gpointer data)
+{
+    Taskbar* taskbar = (Taskbar *) data;
+  
+    if (event->detail != GDK_NOTIFY_INFERIOR)
+    {
+	gtk_widget_hide (taskbar->frame);
+	gtk_widget_set_size_request(GTK_WIDGET(taskbar->win), taskbar->width, HIDDEN_HEIGHT);
+	gtk_window_move(GTK_WINDOW(taskbar->win), taskbar->x, taskbar->y);
+    }
+    return FALSE;
+}
+
+static void panel_destroy(GtkWidget * widget, gpointer data)
+{
+    g_assert (data != NULL);
+    g_free(data);
+}
+
+int main(int argc, char **argv)
+{
+    SessionClient *client_session;
+    DesktopMargins margins;
+    NetkScreen *screen;
+    Taskbar *taskbar;
+    
     gtk_init(&argc, &argv);
 
     client_session = client_session_new(argc, argv, NULL /* data */ , SESSION_RESTART_IF_RUNNING, 60);
@@ -33,47 +75,62 @@ int main(int argc, char **argv)
     {
         g_message("Cannot set desktop margins");
     }
-
-    win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_stick(GTK_WINDOW(win));
-    netk_gtk_window_set_dock_type(GTK_WINDOW(win));
-    gtk_window_stick(GTK_WINDOW(win));
-    gtk_window_set_title(GTK_WINDOW(win), "Task List");
-    gtk_window_set_decorated(GTK_WINDOW(win), FALSE);
-    gtk_window_set_resizable(GTK_WINDOW(win), FALSE);
-    gtk_window_set_default_size(GTK_WINDOW(win), gdk_screen_width() - margins.left - margins.right, FIXED_HEIGHT);
-    gtk_widget_set_size_request(GTK_WIDGET(win), gdk_screen_width() - margins.left - margins.right, FIXED_HEIGHT);
-    gtk_window_move(GTK_WINDOW(win), margins.left, margins.top);
-    margins.top += FIXED_HEIGHT;
     
-    /* quit on window close */
-    g_signal_connect(G_OBJECT(win), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    taskbar = g_new(Taskbar, 1);
+    taskbar->x = margins.left;
+    taskbar->y = margins.top;
+    taskbar->width = gdk_screen_width() - margins.left - margins.right;
+    taskbar->height = FIXED_HEIGHT;
 
-    frame = gtk_frame_new(NULL);
-    gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_OUT);
-    gtk_container_add(GTK_CONTAINER(win), frame);
+    taskbar->win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_stick(GTK_WINDOW(taskbar->win));
+    netk_gtk_window_set_dock_type(GTK_WINDOW(taskbar->win));
+    gtk_window_set_title(GTK_WINDOW(taskbar->win), "Task List");
+    gtk_window_set_decorated(GTK_WINDOW(taskbar->win), FALSE);
+    gtk_window_set_resizable(GTK_WINDOW(taskbar->win), FALSE);
+    gtk_window_set_default_size(GTK_WINDOW(taskbar->win), taskbar->width, taskbar->height);
+    gtk_widget_set_size_request(GTK_WIDGET(taskbar->win), taskbar->width, taskbar->height);
+    gtk_window_move(GTK_WINDOW(taskbar->win), taskbar->x, taskbar->y);
+    g_signal_connect (G_OBJECT(taskbar->win), "destroy", G_CALLBACK(panel_destroy), taskbar);
+    margins.top += HIDDEN_HEIGHT;
+    
+    taskbar->eventbox = gtk_event_box_new();
+    gtk_container_add(GTK_CONTAINER(taskbar->win), taskbar->eventbox);
+    gtk_widget_add_events (taskbar->eventbox, GDK_ENTER_NOTIFY_MASK | GDK_LEAVE_NOTIFY_MASK);
+    g_signal_connect (G_OBJECT(taskbar->eventbox), "enter_notify_event", G_CALLBACK (panel_enter), taskbar);
+    g_signal_connect (G_OBJECT(taskbar->eventbox), "leave_notify_event", G_CALLBACK (panel_leave), taskbar);
+    
+    taskbar->frame = gtk_frame_new(NULL);
+    gtk_frame_set_shadow_type(GTK_FRAME(taskbar->frame), GTK_SHADOW_OUT);
+    gtk_container_add(GTK_CONTAINER(taskbar->eventbox), taskbar->frame);
 
-    hbox = gtk_hbox_new (FALSE, 1);
-    gtk_container_add (GTK_CONTAINER (frame), hbox);
-    gtk_widget_show (hbox);
+    taskbar->hbox = gtk_hbox_new (FALSE, 1);
+    gtk_container_add (GTK_CONTAINER (taskbar->frame), taskbar->hbox);
 
-    tasklist = netk_tasklist_new(screen);
-    netk_tasklist_set_grouping(NETK_TASKLIST(tasklist), NETK_TASKLIST_ALWAYS_GROUP);
-    netk_tasklist_set_grouping_limit(NETK_TASKLIST(tasklist), 0);
-    gtk_widget_set_size_request (GTK_WIDGET (tasklist), -1, FIXED_HEIGHT);
-    gtk_box_pack_start (GTK_BOX (hbox), tasklist, TRUE, TRUE, 0);
+    taskbar->tasklist = netk_tasklist_new(screen);
+    netk_tasklist_set_grouping(NETK_TASKLIST(taskbar->tasklist), NETK_TASKLIST_ALWAYS_GROUP);
+    netk_tasklist_set_grouping_limit(NETK_TASKLIST(taskbar->tasklist), 0);
+    gtk_widget_set_size_request (GTK_WIDGET (taskbar->tasklist), -1, FIXED_HEIGHT);
+    gtk_box_pack_start (GTK_BOX (taskbar->hbox), taskbar->tasklist, TRUE, TRUE, 0);
 
-    pager = netk_pager_new(screen);
-    netk_pager_set_orientation(NETK_PAGER(pager), GTK_ORIENTATION_HORIZONTAL);
-    netk_pager_set_n_rows(NETK_PAGER(pager), 1);
-    gtk_widget_set_size_request (GTK_WIDGET (pager), -1, FIXED_HEIGHT);
-    gtk_box_pack_end (GTK_BOX (hbox), pager, FALSE, FALSE, 0);
+    taskbar->pager = netk_pager_new(screen);
+    netk_pager_set_orientation(NETK_PAGER(taskbar->pager), GTK_ORIENTATION_HORIZONTAL);
+    netk_pager_set_n_rows(NETK_PAGER(taskbar->pager), 1);
+    gtk_widget_set_size_request (GTK_WIDGET (taskbar->pager), -1, FIXED_HEIGHT);
+    gtk_box_pack_end (GTK_BOX (taskbar->hbox), taskbar->pager, FALSE, FALSE, 0);
 
-    gtk_widget_show(tasklist);
-    gtk_widget_show(pager);
-    gtk_widget_show(frame);
-    gtk_widget_show(win);
-    netk_set_desktop_margins(GDK_WINDOW_XWINDOW(win->window), &margins);
+    gtk_widget_show (taskbar->tasklist);
+    gtk_widget_show (taskbar->pager);
+    gtk_widget_show (taskbar->hbox);
+    gtk_widget_show (taskbar->frame);
+    gtk_widget_show (taskbar->eventbox);
+    gtk_widget_show (taskbar->win);
+
+    gtk_widget_hide (taskbar->frame);
+    gtk_widget_set_size_request(GTK_WIDGET(taskbar->win), taskbar->width, HIDDEN_HEIGHT);
+    gtk_window_move(GTK_WINDOW(taskbar->win), taskbar->x, taskbar->y);
+
+    netk_set_desktop_margins(GDK_WINDOW_XWINDOW(taskbar->win->window), &margins);
 
     gtk_main();
 
