8e5023203c5bab9f0d7f75f93146c13da8c52b91
Jasper Huijsmans <jbhuijsmans@home.nl>
2003-03-12 13:19:01 +0000

    Safe terminal option in history and allow xfrun to open directories. Patch by James Tappin.
    (Old svn revision: 6495)

--- xfrun/xfrun.c
+++ xfrun/xfrun.c
@@ -17,11 +17,17 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#undef GTK_DISABLE_DEPRECATED
+
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#include <libxfcegui4/dialogs.h>
 
 #ifndef PATH_MAX
 #define DEFAULT_LENGTH 1024
@@ -32,160 +38,155 @@
 #define DEFAULT_LENGTH PATH_MAX
 #endif
 #endif
-                                                                                                                          
+
 #define _(x) x
 #define N_(x) x
 
-#define HFILE ".xfce4/xfrun_history"
+#define XFCEDIR ".xfce4"
+#define HFILE "xfrun_history"
 
 #define MAXHISTORY 10
 
+typedef struct
+{
+    gchar *command;
+    gboolean in_terminal;
+}
+XFCommand;
+
 GtkWidget *dialog;
+GtkWidget *checkbox;
+GList *history = NULL;
+char *fileman = NULL;
 
-/*  Button with text and stock icon
- *  -------------------------------
- *  Taken from ROX Filer (http://rox.sourceforge.net)
- *  by Thomas Leonard
-*/
-GtkWidget *mixed_button_new(const char *stock, const char *message)
+void set_history_checkbox(GtkList *list, GtkWidget *child)
 {
-    GtkWidget *button, *align, *image, *hbox, *label;
-
-    button = gtk_button_new();
-    label = gtk_label_new_with_mnemonic(message);
-    gtk_label_set_mnemonic_widget(GTK_LABEL(label), button);
-
-    image = gtk_image_new_from_stock(stock, GTK_ICON_SIZE_BUTTON);
-    hbox = gtk_hbox_new(FALSE, 2);
-
-    align = gtk_alignment_new(0.5, 0.5, 0.0, 0.0);
-
-    gtk_box_pack_start(GTK_BOX(hbox), image, FALSE, FALSE, 0);
-    gtk_box_pack_end(GTK_BOX(hbox), label, FALSE, FALSE, 0);
-
-    gtk_container_add(GTK_CONTAINER(button), align);
-    gtk_container_add(GTK_CONTAINER(align), hbox);
-    gtk_widget_show_all(align);
-
-    return button;
+    gint ipos = gtk_list_child_position(list, child);
+    GList *hitem = g_list_nth(history, ipos);
+    XFCommand *current = hitem -> data;
+    
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(checkbox),
+				 current->in_terminal);
 }
 
-/* from xfce4 */
-void report_error(const char *text, GtkWidget *parent)
+static gboolean do_run(const char *cmd, gboolean in_terminal)
 {
-    GtkWidget *dlg;
-
-    dlg = gtk_message_dialog_new(parent ? GTK_WINDOW(parent) : NULL,
-                                    GTK_DIALOG_MODAL |
-                                    GTK_DIALOG_DESTROY_WITH_PARENT,
-                                    GTK_MESSAGE_WARNING, GTK_BUTTONS_CLOSE,
-                                    text);
-
-    gtk_window_set_position(GTK_WINDOW(dlg), GTK_WIN_POS_CENTER);
-
-    gtk_dialog_run(GTK_DIALOG(dlg));
-    gtk_widget_destroy(dlg);
-}
-
-/* Executing commands. Taken from xfce4. */
-
-static gboolean exec_cmd(const char *cmd, gboolean in_terminal)
-{
-    GError *error = NULL;       /* this must be NULL to prevent crash :( */
     gchar *execute;
+    gboolean success;
 
     g_return_val_if_fail(cmd != NULL, FALSE);
 
-    if(in_terminal)
-        execute = g_strdup_printf("xfterm -e %s", cmd);
-    else
-        execute = g_strdup(cmd);
-
-    if(!g_spawn_command_line_async(execute, &error))
+    /* open directory in terminal or file manager */
+    if (g_file_test (cmd, G_FILE_TEST_IS_DIR))
     {
-        gchar *msg;
-
-        msg = g_strcompress(error->message);
-
-        report_error(msg, dialog);
-        
-	g_error_free(error);
-        g_free(execute);
-        g_free(msg);
-	
-	return FALSE;
+	if(in_terminal)
+	    execute = g_strconcat("xfterm4 ", cmd, NULL);
+	else 
+	    execute = g_strconcat(fileman, " ", cmd, NULL);
     }
-    
+    else
+    {
+	if(in_terminal)
+	    execute = g_strconcat("xfterm4 -e ", cmd, NULL);
+	else
+	    execute = g_strdup(cmd);
+    }
+
+    success = exec_command(execute);
     g_free(execute);
-    return TRUE;
+    
+    return success;
+}
+
+static char *get_fileman(void)
+{
+    const char *var = g_getenv("FILEMAN");
+
+    if (var && strlen(var))
+	return g_strdup(var);
+    else
+	return g_strdup("xffm");	    
 }
 
 GList *get_history(void)
 {
     FILE *fp;
     const char *home = g_get_home_dir();
-    char *hfile = g_strconcat(home, "/", HFILE, NULL);
-    GList *cbtemp = NULL;
+    char *hfile = g_build_filename(home, XFCEDIR, HFILE, NULL);
     char line[DEFAULT_LENGTH];
     char *check;
+    GList *cbtemp = NULL;
+    XFCommand *current;
+
     int i = 0;
 
     if(!(fp = fopen(hfile, "r")))
     {
         g_free(hfile);
-        cbtemp = g_list_append(cbtemp, "");
 
-        return cbtemp;
+        return NULL;
     }
 
     line[DEFAULT_LENGTH - 1] = '\0';
 
-    /* no more than 10 history items */
+    /* no more than MAXHISTORY history items */
     for(i = 0; i < MAXHISTORY && fgets(line, DEFAULT_LENGTH - 1, fp); i++)
     {
         if((line[0] == '\0') || (line[0] == '\n'))
             break;
 
+        current = g_new0(XFCommand, 1);
+	
         if((check = strrchr(line, '\n')))
             *check = '\0';
 
-        cbtemp = g_list_append(cbtemp, g_strdup(line));
+	if ((check = strrchr(line, ' ')))
+	{
+	    *check = '\0';
+	    check++;
+	    current->in_terminal = (atoi(check) != 0);
+	} 
+	else 
+	{
+	    current->in_terminal = FALSE;
+	}
+	
+	current->command = g_strdup(line);
+        cbtemp = g_list_append(cbtemp, current);
     }
 
     g_free(hfile);
     fclose(fp);
 
-    if(!cbtemp)
-        cbtemp = g_list_append(cbtemp, "");
-
     return cbtemp;
 }
 
-void put_history(const char *newest, GList * cb)
+void put_history(const char *newest, gboolean in_terminal, GList * cb)
 {
     FILE *fp;
     const char *home = g_get_home_dir();
-    char *hfile = g_strconcat(home, "/", HFILE, NULL);
+    char *hfile = g_build_filename(home, XFCEDIR, HFILE, NULL);
     GList *node;
     int i;
 
     if(!(fp = fopen(hfile, "w")))
     {
-        g_print("xfrun: Could not write history to file %s\n", hfile);
+        g_warning("xfrun4: Could not write history to file %s\n", hfile);
         g_free(hfile);
         return;
     }
 
-    fprintf(fp, "%s\n", newest);
+    fprintf(fp, "%s %d\n", newest, in_terminal);
     i = 1;
 
     for(node = cb; node && i < MAXHISTORY; node = node->next)
     {
-        char *cmd = (char *)node->data;
+	XFCommand *current =  node->data;
 
-        if(cmd && *cmd != '\0' && (strcmp(cmd, newest) != 0))
+        if(current->command && strlen(current->command) &&
+	   (strcmp(current->command, newest) != 0))
         {
-            fprintf(fp, "%s\n", cmd);
+            fprintf(fp, "%s %d\n", current->command, current->in_terminal);
             i++;
         }
     }
@@ -194,20 +195,29 @@ void put_history(const char *newest, GList * cb)
     g_free(hfile);
 }
 
+static void free_hitem(XFCommand *hitem)
+{
+    g_free(hitem->command);
+    g_free(hitem);
+}
+
 int main(int argc, char **argv)
 {
     GtkWidget *button;
-    GtkWidget *combo_entry;
-    GList *history;
-    GtkWidget *checkbox;
+    GtkWidget *combo_entry, *combo_list;
+    GList *hitem, *hstrings=NULL;
     GtkWidget *vbox;
     GtkWidget *combo;
+    XFCommand *current;
 
     gtk_init(&argc, &argv);
 
     history = get_history();
 
-    dialog = gtk_dialog_new_with_buttons(_("Run program"), NULL, 0, NULL);
+    fileman = get_fileman();
+    
+    dialog = gtk_dialog_new_with_buttons(_("Run program"), NULL, 
+					 GTK_DIALOG_NO_SEPARATOR, NULL);
     
     button = gtk_button_new_from_stock(GTK_STOCK_CANCEL);
     gtk_widget_show(button);
@@ -223,27 +233,42 @@ int main(int argc, char **argv)
     gtk_window_set_default_size(GTK_WINDOW(dialog), 400, 10);
     gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_MOUSE);
 
-    vbox = gtk_vbox_new(FALSE, 4);
+    vbox = gtk_vbox_new(FALSE, 5);
     gtk_widget_show(vbox);
     gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), vbox, TRUE, TRUE, 0);
-    gtk_container_set_border_width(GTK_CONTAINER(vbox), 5);
+    gtk_container_set_border_width(GTK_CONTAINER(vbox), 6);
 
     combo = gtk_combo_new();
-    combo_entry = GTK_COMBO(combo)->entry;
-    gtk_combo_set_popdown_strings(GTK_COMBO(combo), history);
-    gtk_box_pack_start(GTK_BOX(vbox), combo, TRUE, TRUE, 2);
+    
+    for (hitem = history ; hitem ; hitem=hitem->next) 
+    {
+	current=hitem->data;
+	hstrings=g_list_append(hstrings,current->command);
+    }
+    
+    gtk_combo_set_popdown_strings(GTK_COMBO(combo), hstrings);
+    g_list_free(hstrings);
+    gtk_box_pack_start(GTK_BOX(vbox), combo, TRUE, TRUE, 0);
     gtk_widget_show(combo);
+		     
+    combo_entry = GTK_COMBO(combo)->entry;
+    combo_list = GTK_COMBO(combo)->list;
 
     gtk_combo_disable_activate(GTK_COMBO(combo));
     g_object_set(G_OBJECT(combo_entry), "activates-default", TRUE, NULL);
 
     checkbox = gtk_check_button_new_with_mnemonic(_("Run in _terminal"));
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(checkbox), FALSE);
+    current=history->data;
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(checkbox),
+				 current->in_terminal);
     gtk_widget_show(checkbox);
-    gtk_box_pack_start(GTK_BOX(vbox), checkbox, TRUE, TRUE, 2);
+    gtk_box_pack_start(GTK_BOX(vbox), checkbox, TRUE, TRUE, 0);
 
     gtk_editable_select_region(GTK_EDITABLE(combo_entry), 0, -1);
     gtk_widget_grab_focus(combo_entry);
+
+    g_signal_connect(G_OBJECT(combo_list), "select_child",
+		     G_CALLBACK(set_history_checkbox), NULL);
 
     while (1)
     {
@@ -260,17 +285,22 @@ int main(int argc, char **argv)
 
 	    in_terminal = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(checkbox));
 
-	    if (exec_cmd(command, in_terminal))
+	    if (do_run(command, in_terminal))
 	    {
-		put_history(command, history);
+		put_history(command, in_terminal, history);
 		break;
 	    }
 	}
 	else
+	{
 	    break;
+	}
     }
     
     gtk_widget_destroy(dialog);
+    g_free(fileman);
+    g_list_foreach(history, (GFunc)free_hitem, NULL);
+    g_list_free(history);
 
     return 0;
 }
