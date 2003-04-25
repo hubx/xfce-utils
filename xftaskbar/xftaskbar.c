6b6b353506afa75e5713935c42d3e38815941ab9
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2003-04-25 22:35:18 +0000

    Fix missing xinerama init *sigh*
    (Old svn revision: 6535)

--- xftaskbar/xftaskbar.c
+++ xftaskbar/xftaskbar.c
@@ -44,7 +44,7 @@
 #define HIDDEN_HEIGHT 5
 #define TOP TRUE
 #define BOTTOM FALSE
-#define DEFAULT_HEIGHT	30
+#define DEFAULT_HEIGHT  30
 
 static McsClient *client = NULL;
 
@@ -113,7 +113,7 @@ static gint taskbar_get_thickness(Taskbar *taskbar)
     style = gtk_widget_get_style(taskbar->win);
     if (style)
     {
-	return style->ythickness;
+        return style->ythickness;
     }
     g_warning(_("Cannot get initial style"));
     return 2;
@@ -124,7 +124,7 @@ static gint taskbar_get_height(Taskbar *taskbar)
     g_return_val_if_fail (taskbar != NULL, DEFAULT_HEIGHT);
     if (taskbar->hidden)
     {
-	return (2 * taskbar_get_thickness(taskbar) + 1);
+        return (2 * taskbar_get_thickness(taskbar) + 1);
     }
     return taskbar->height;
 }
@@ -137,11 +137,11 @@ static void taskbar_update_margins(Taskbar *taskbar)
     g_return_if_fail (taskbar != NULL);
     if (taskbar->autohide)
     {
-	height = (2 * taskbar_get_thickness(taskbar) + 1);
+        height = (2 * taskbar_get_thickness(taskbar) + 1);
     }
     else
     {
-	height = taskbar->height;
+        height = taskbar->height;
     }
     
     margins.left = 0;
@@ -162,15 +162,15 @@ static void taskbar_toggle_autohide(Taskbar *taskbar)
     g_return_if_fail (taskbar != NULL);
     if (taskbar->autohide)
     {
-	gtk_widget_hide (taskbar->frame);
+        gtk_widget_hide (taskbar->frame);
         taskbar->hidden = TRUE;
-	taskbar_position(taskbar);
+        taskbar_position(taskbar);
     }
     else
     {
-	gtk_widget_show (taskbar->frame);
+        gtk_widget_show (taskbar->frame);
         taskbar->hidden = FALSE;
-	taskbar_position(taskbar);
+        taskbar_position(taskbar);
     }
     taskbar_update_margins(taskbar);
 }
@@ -180,11 +180,11 @@ static void taskbar_toggle_pager(Taskbar *taskbar)
     g_return_if_fail (taskbar != NULL);
     if (taskbar->show_pager)
     {
-	gtk_widget_show (taskbar->pager);
+        gtk_widget_show (taskbar->pager);
     }
     else
     {
-	gtk_widget_hide (taskbar->pager);
+        gtk_widget_hide (taskbar->pager);
     }
 }
 
@@ -193,11 +193,11 @@ static void taskbar_toggle_tray(Taskbar *taskbar)
     g_return_if_fail (taskbar != NULL);
     if (taskbar->show_tray)
     {
-	gtk_widget_show (taskbar->iconbox);
+        gtk_widget_show (taskbar->iconbox);
     }
     else
     {
-	gtk_widget_hide (taskbar->iconbox);
+        gtk_widget_hide (taskbar->iconbox);
     }
 }
 
@@ -208,7 +208,7 @@ static void taskbar_change_size(Taskbar *taskbar, int height)
     {
         taskbar->height = height;
         gtk_widget_set_size_request(GTK_WIDGET(taskbar->pager), -1, taskbar->height - 2 * taskbar_get_thickness(taskbar));
-	taskbar_position(taskbar);
+        taskbar_position(taskbar);
         taskbar_update_margins(taskbar);
     }
 }
@@ -220,14 +220,14 @@ static gboolean taskbar_size_allocate (GtkWidget *widget, GtkAllocation *allocat
     g_assert (data != NULL);
     if ((allocation) && (widget == taskbar->win))
     {
-	if (taskbar->position == TOP)
-	{
+        if (taskbar->position == TOP)
+        {
             taskbar->y = 0;
-	}
-	else
-	{
+        }
+        else
+        {
             taskbar->y = gdk_screen_height() - allocation->height;
-	}
+        }
         gtk_window_move(GTK_WINDOW(taskbar->win), taskbar->x, taskbar->y);
     }
     return FALSE;
@@ -243,9 +243,9 @@ static gboolean taskbar_enter (GtkWidget *widget, GdkEventCrossing *event, gpoin
     }
     if (event->detail != GDK_NOTIFY_INFERIOR)
     {
-	gtk_widget_show (taskbar->frame);
+        gtk_widget_show (taskbar->frame);
         taskbar->hidden = FALSE;
-	taskbar_position(taskbar);
+        taskbar_position(taskbar);
     }
   
     return FALSE;
@@ -261,9 +261,9 @@ static gboolean taskbar_leave (GtkWidget *widget, GdkEventCrossing *event, gpoin
     }
     if (event->detail != GDK_NOTIFY_INFERIOR)
     {
-	gtk_widget_hide (taskbar->frame);
+        gtk_widget_hide (taskbar->frame);
         taskbar->hidden = TRUE;
-	taskbar_position(taskbar);
+        taskbar_position(taskbar);
     }
     return FALSE;
 }
@@ -287,28 +287,28 @@ static void notify_cb(const char *name, const char *channel_name, McsAction acti
                 if (!strcmp(name, "Taskbar/Position"))
                 {
                     taskbar->position = setting->data.v_int ? TOP : BOTTOM;
-		    taskbar_position(taskbar);
-		    taskbar_update_margins(taskbar);
+                    taskbar_position(taskbar);
+                    taskbar_update_margins(taskbar);
                 }
                 else if (!strcmp(name, "Taskbar/AutoHide"))
                 {
                     taskbar->autohide = setting->data.v_int ? TRUE : FALSE;
-		    taskbar_toggle_autohide(taskbar);
+                    taskbar_toggle_autohide(taskbar);
                 }
                 else if (!strcmp(name, "Taskbar/ShowPager"))
                 {
                     taskbar->show_pager = setting->data.v_int ? TRUE : FALSE;
-		    taskbar_toggle_pager(taskbar);
+                    taskbar_toggle_pager(taskbar);
                 }
                 else if(!strcmp(name, "Taskbar/ShowTray"))
                 {
                     taskbar->show_tray = setting->data.v_int ? TRUE : FALSE;
-		    taskbar_toggle_tray(taskbar);
+                    taskbar_toggle_tray(taskbar);
                 }
                 else if(!strcmp(name, "Taskbar/ShowAllTasks"))
                 {
                     taskbar->all_tasks = setting->data.v_int ? TRUE : FALSE;
-		    netk_tasklist_set_include_all_workspaces(NETK_TASKLIST(taskbar->tasklist), taskbar->all_tasks);
+                    netk_tasklist_set_include_all_workspaces(NETK_TASKLIST(taskbar->tasklist), taskbar->all_tasks);
                 }
                 else if (!strcmp(name, "Taskbar/Height"))
                 {
@@ -328,15 +328,13 @@ static void taskbar_destroy(GtkWidget * widget, gpointer data)
     g_free(data);
 }
 
-static void
-icon_docked(XfceSystemTray *tray, GtkWidget *icon, GtkBox *iconbox)
+static void icon_docked(XfceSystemTray *tray, GtkWidget *icon, GtkBox *iconbox)
 {
-	gtk_box_pack_start(iconbox, icon, FALSE, FALSE, 5);
-	gtk_widget_show(icon);
+    gtk_box_pack_start(iconbox, icon, FALSE, FALSE, 5);
+    gtk_widget_show(icon);
 }
 
-static void
-icon_undocked(XfceSystemTray *tray, GtkWidget *icon, GtkBox *iconbox)
+static void icon_undocked(XfceSystemTray *tray, GtkWidget *icon, GtkBox *iconbox)
 {
 }
 
@@ -350,6 +348,7 @@ int main(int argc, char **argv)
     Display *dpy;
     int scr;
     int left, right;
+    gboolean use_xinerama;
 
 #ifdef ENABLE_NLS
     /* This is required for UTF-8 at least - Please don't remove it */
@@ -373,11 +372,12 @@ int main(int argc, char **argv)
     /* because the pager doesn't respond to signals at the moment */
     netk_screen_force_update(screen);
 
-    if(!netk_get_desktop_margins(DefaultScreenOfDisplay(GDK_DISPLAY()), &margins))
+    if(!netk_get_desktop_margins(DefaultScreenOfDisplay(dpy), &margins))
     {
         g_message(_("Cannot get desktop margins"));
     }
     
+    use_xinerama = xineramaInit(dpy);
     left = isLeftMostHead(dpy, scr, 0, 0) ? margins.left : 0;
     right = isRightMostHead(dpy, scr, 0, 0) ? margins.right : 0;
     
@@ -428,19 +428,17 @@ int main(int argc, char **argv)
 
     taskbar->iconbox = gtk_hbox_new(TRUE, 5);
     gtk_box_pack_start(GTK_BOX(taskbar->hbox), taskbar->iconbox, FALSE, FALSE,
-		    0);
+                    0);
 
     taskbar->tray = xfce_system_tray_new();
 
-    g_signal_connect(taskbar->tray, "icon_docked",
-		    G_CALLBACK(icon_docked), taskbar->iconbox);
-    g_signal_connect(taskbar->tray, "icon_undocked",
-		    G_CALLBACK(icon_undocked), taskbar->iconbox);
+    g_signal_connect(taskbar->tray, "icon_docked", G_CALLBACK(icon_docked), taskbar->iconbox);
+    g_signal_connect(taskbar->tray, "icon_undocked", G_CALLBACK(icon_undocked), taskbar->iconbox);
 
-    if (!xfce_system_tray_register(taskbar->tray, gdk_screen_get_default(),
-			    &error)) {
-	    xfce_err("Unable to register system tray: %s", error->message);
-	    g_error_free(error);
+    if (!xfce_system_tray_register(taskbar->tray, gdk_screen_get_default(), &error)) 
+    {
+        xfce_err("Unable to register system tray: %s", error->message);
+        g_error_free(error);
     }
 
     gtk_widget_show (taskbar->iconbox);
@@ -450,8 +448,7 @@ int main(int argc, char **argv)
     taskbar_change_size(taskbar, DEFAULT_HEIGHT);
     taskbar_position(taskbar);
 
-    client = mcs_client_new(GDK_DISPLAY(), XDefaultScreen(GDK_DISPLAY()),
-		    notify_cb, watch_cb, taskbar);
+    client = mcs_client_new(GDK_DISPLAY(), XDefaultScreen(GDK_DISPLAY()), notify_cb, watch_cb, taskbar);
     if(client)
     {
         mcs_client_add_channel(client, CHANNEL);
@@ -463,5 +460,6 @@ int main(int argc, char **argv)
 
     gtk_main();
 
+    xineramaFree();
     return 0;
 }
