de04c129211383dfa2e6a371f99f88586bcaf7fa
Jean-François Wauthy <pollux@xfce.org>
2006-07-30 08:31:55 +0000

    Updated Czech translations
    (Old svn revision: 22562)

---
+++ xfce4-about/INFO.cs
@@ -0,0 +1,43 @@
+Prostředí Xfce 4
+verze @VERSION@
+
+Xfce je skupina programů, které společně poskytují plně použitelné
+prostředí. Součástmi Xfce jsou tyto programy:
+
+o Správce oken (xfwm4)
+   stará se o umístění oken na obrazovce
+
+o Panel (xfce4-panel)
+   spouštěče programů, menu, hodiny, přepínač virtuální pracovních ploch
+   a další
+
+o Správce plochy (xfdesktop)
+   nastavuje barvu nebo obrázek pozadí a poskytuje přístup do menu při
+   kliknutí na plochu
+
+o Správce souborů (xffm)
+   rychlý správce souborů
+
+o Správce sezení (xfce4-session)
+   obnovuje Vaše sezení po spuštění a umožňuje Vám vypnout počítač z Xfce
+
+o Nástroje
+   xfprint4: tisk souborů
+   xfrun4: spouštění programů
+   xftaskbar4: jednoduchý panel úloh s volitelným přepínačem ploch
+               a systémovou oblastí
+   xfce4-iconbox: zobrazuje minimalizované aplikace v oblasti pro ikony
+
+
+Xfce 4 je také vývojová platforma, poskytující mnoho knihoven, které
+umožňují programátorům vytvářet aplikace, které „zapadnou” do tohoto prostředí.
+
+Komponenty Xfce 4 jsou uvolněny pod svobodnými a otevřenými licencemi GPL či BSDL
+pro aplikace a LGPL nebo BSDL pro knihovny. Více informací naleznete v dokumentaci,
+zdrojových kódech nebo na webových stránkách xfce (http://www.xfce.org).
+
+
+Děkujeme za Váš zájem o Xfce.
+
+                   -- Vývojový tým Xfce --
+
