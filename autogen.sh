25cf3bd1232c21c9d2a40c9f7342ddd4c34c5704
Brian Tarricone <brian@tarricone.org>
2008-08-25 13:15:37 +0000

    get revision from git-svn
    (Old svn revision: 27575)

--- autogen.sh
+++ autogen.sh
@@ -29,7 +29,16 @@ EOF
 
 # substitute revision and linguas
 linguas=`sed -e '/^#/d' po/LINGUAS`
-revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n", $2}'`
+if [ -d .git/svn ]; then
+  revision=$(git svn find-rev trunk 2>/dev/null ||
+             git svn find-rev origin/trunk 2>/dev/null ||
+             git svn find-rev HEAD 2>/dev/null ||
+             git svn find-rev master 2>/dev/null)
+elif [ -d .svn ]; then
+  revision=$(LC_ALL=C svn info $0 | awk '/^Revision: /{ printf "%05d\n", $2; }')
+else
+  revision=UNKNOWN
+fi
 sed -e "s/@LINGUAS@/${linguas}/g" \
     -e "s/@REVISION@/${revision}/g" \
     < "configure.in.in" > "configure.in"
